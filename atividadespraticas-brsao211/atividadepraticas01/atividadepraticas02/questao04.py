92466f1bf649ea1ddb891a02deefaa3749445b82
Eunice Araujo <67357363+EuniceMAraujo@users.noreply.github.com>
2026-01-23 19:27:26 -0300

    Delete atividadespraticas-brsao211/atividadepraticas01/atividadepraticas02 directory

--- atividadespraticas-brsao211/atividadepraticas01/atividadepraticas02/questao04.py
+++
@@ -1,9 +0,0 @@
-distancia = 300
-combustivel = 25
-
-consumo = distancia / combustivel
-
-print("Distância percorrida:", distancia, "km")
-print("Combustível gasto:", combustivel, "litros")
-
-print("Consumo médio:", round(consumo, 2), "km/l")
